bf52a42d68661a34e42a3f04afc39770d776acc8
Muhand Hasaneen <ma5749@fayoum.edu.eg>
2025-06-22 00:57:15 +0300

    adjusted package.json and moved server to api/index.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,8 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log("🔍 MONGO_URI ===>", process.env.MONGO_URI);
+
   } catch (err) {
     console.error(`Error: ${err.message}`);
     process.exit(1);
