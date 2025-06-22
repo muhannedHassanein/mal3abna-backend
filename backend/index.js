c9a8d061bacb752afc1fc1a9fa33a0ce5aa0ba90
Muhand Hasaneen <ma5749@fayoum.edu.eg>
2025-06-22 20:11:57 +0300

    Replaced server.js with index.js and added vercel.json

--- backend/index.js
+++ backend/index.js
@@ -12,18 +12,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// مسار الصور
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-// الراوتات
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/pitches", require("./routes/pitchRoutes"));
 
-// راوت افتراضي
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-// تشغيل السيرفر
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
